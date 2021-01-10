8bc9254e00b341a1efdb87ec7c1fa29ebf322ab5
Sachin Koparde <contactsachinkoparde@gmail.com>
2021-01-10 13:20:06 +0530

    added set.kt

---
+++ src/set.kt
@@ -0,0 +1,11 @@
+fun main() {
+    val numbers = listOf(0, 3, 8, 4, 0, 5, 5, 8, 9, 2)
+    println("list: ${numbers}")
+    println("sorted: ${numbers.sorted()}")
+    val setOfNumbers = numbers.toSet()
+    println("set: ${setOfNumbers}")
+    val set1 = setOf(1,2,3)
+    val set2 = setOf(3,2,1)
+    println("set1 == set2? ${set1 == set2}")
+	//In set order of elements doesn't matter
+}
